80d1fa502243ef222899f63a3b3a9eb98243f88b
Aurelien <apajadon@hotmail.com>
2026-02-18 20:07:49 +0100

    feat(models): adding MainViewModel

---
+++ EmbeddedNetworkLab/UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace EmbeddedNetworkLab.UI.ViewModel
+{
+	public partial class MainViewModel : ObservableObject
+	{
+		[ObservableProperty]
+		private string selectedModuleTitle = "Select a module";
+
+		[ObservableProperty]
+		private string consoleText = "Console initialized...";
+	}
+}
+
