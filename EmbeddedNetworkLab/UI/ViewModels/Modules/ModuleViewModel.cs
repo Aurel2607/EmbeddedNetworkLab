9e1dada6e1b35fe79a19e9e3aa36e5d3501df48f
Aurelien <apajadon@hotmail.com>
2026-02-19 12:00:07 +0100

    feat(model): adding ModuleViewModel & ThroughputViewModel

---
+++ EmbeddedNetworkLab/UI/ViewModels/Modules/ModuleViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace EmbeddedNetworkLab.UI.ViewModels
+{
+	public abstract partial class ModuleViewModel : ObservableObject
+	{
+		public abstract string Name { get; }
+
+		[ObservableProperty]
+		private bool isRunning;
+
+		protected bool TryStart()
+		{
+			if (IsRunning)
+				return false;
+
+			IsRunning = true;
+			return true;
+		}
+
+		protected void StopExecution()
+		{
+			IsRunning = false;
+		}
+
+		// This method is called whenever the IsRunning property changes,
+		// allowing derived classes to react to the change in running state.
+		partial void OnIsRunningChanged(bool value)
+		{
+			OnRunningStateChanged(value);
+		}
+
+		// Allow derived classes to override this method to react to changes in the running state
+		protected virtual void OnRunningStateChanged(bool isRunning)
+		{
+		}
+	}
+}
---
+++ EmbeddedNetworkLab/UI/ViewModels/Modules/ThroughputViewModel.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using EmbeddedNetworkLab.Core.Services;
+using System.Windows;
+
+namespace EmbeddedNetworkLab.UI.ViewModels.Modules
+{
+	public partial class ThroughputViewModel : ModuleViewModel
+	{
+		private readonly IThroughputService _service;
+
+		public override string Name => "Throughput Test";
+
+		[ObservableProperty]
+		private double currentRate;
+
+		public ThroughputViewModel(IThroughputService service)
+		{
+			_service = service;
+
+			_service.RateUpdated += rate =>
+			{
+				Application.Current.Dispatcher.Invoke(() =>
+				{
+					CurrentRate = rate;
+				});
+			};
+		}
+
+		[RelayCommand(CanExecute = nameof(CanStart))]
+		private void Start()
+		{
+			if (!TryStart())
+				return;
+
+			_service.Start();
+		}
+
+		private bool CanStart()
+		{
+			return !IsRunning;
+		}
+
+		protected override void OnRunningStateChanged(bool isRunning)
+		{
+			StartCommand.NotifyCanExecuteChanged();
+		}
+	}
+}
